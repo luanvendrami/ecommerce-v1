ca37bb3abe8fb41d8154b0b7b225ba6339952979
Luan Vendrami <luan.vendrami@hotmail.com>
2025-02-14 00:01:34 -0300

    add metadata in layout and in the home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,25 @@
+import { Metadata } from "next";
 import { Header } from "./components/header";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: 'Home Layout',
+  description: "Teste Layout também",
+  openGraph:{
+    title: 'testando novo Layout',
+    description: "Teste também Layout",
+  },
+  robots:{
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot:{
+      index: true,
+      follow: true,
+    }
+  }
+}
+
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Home teste',
+    description: "Teste também",
+    openGraph:{
+      title: 'testando novo',
+      description: "Teste também",
+    },
+    robots:{
+      index: true,
+      follow: true,
+      nocache: true,
+      googleBot:{
+        index: true,
+        follow: true,
+      }
+    }
+}
+
 export default function Home(){
   return(
     <div>
