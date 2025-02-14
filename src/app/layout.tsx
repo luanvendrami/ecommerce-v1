5aff4488c7f2e02743d1157a3a9b428fa6957d62
Luan Vendrami <luan.vendrami@hotmail.com>
2025-02-13 23:40:24 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import { Header } from "./components/header";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+
+        <Header/>
+
+        {children}
+      </body>
+    </html>
+  );
+}
